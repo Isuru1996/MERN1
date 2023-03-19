f192e4fe4cc19e17d388b5244a71bc1addaf43c1
Isuru1996 <isurug27@gmail.com>
2023-03-19 21:02:23 +0530

    user function complete

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -8,6 +13,25 @@ function Login() {
   });
 
   const { email, password } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -18,25 +42,37 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <section className="heading">
         <h1>
-          <FaSignInAlt />
-          Login
+          <FaSignInAlt /> Login
         </h1>
         <p>Login and start setting goals</p>
       </section>
+
       <section className="form">
         <form onSubmit={onSubmit}>
           <div className="form-group">
             <input
               type="email"
-              name="email"
-              value={email}
               className="form-control"
               id="email"
+              name="email"
+              value={email}
               placeholder="Enter your email"
               onChange={onChange}
             />
@@ -44,17 +80,18 @@ function Login() {
           <div className="form-group">
             <input
               type="password"
-              name="password"
-              value={password}
               className="form-control"
               id="password"
+              name="password"
+              value={password}
               placeholder="Enter password"
               onChange={onChange}
             />
           </div>
+
           <div className="form-group">
             <button type="submit" className="btn btn-block">
-              Login
+              Submit
             </button>
           </div>
         </form>
